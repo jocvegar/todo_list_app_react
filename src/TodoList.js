cf4fe2a9161adf5bfaba261e900dad3aa4740e75
jocvegar <jocvegar@gmail.com>
2019-01-06 20:55:47 -0600

    Initial commit

---
+++ src/TodoList.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import TodoItems from './TodoItems';
+
+class TodoList extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			items: []
+		}
+
+		this.addItem = this.addItem.bind(this);
+	}
+
+	addItem(e) {
+		e.preventDefault()
+		if (this._inputElement.value !== "") {
+			let newItem = {
+				text: this._inputElement.value,
+				key: Date.now()
+			};
+
+			this.setState((prevState) => {
+				return {
+					items: prevState.items.concat(newItem)
+				};
+			})
+		}
+
+		this._inputElement.value = "";
+
+		console.log(this.state.items);
+	}
+
+	render() {
+		return(
+			<div className="todoListMain">
+				<div className="header">
+					<form onSubmit={this.addItem}>
+						<input
+							ref={(a) => this._inputElement = a}
+							type="text"
+							placeholder="Enter Task"/>
+						<button type="submit">Add</button>
+					</form>
+				</div>
+
+				<TodoItems entries={this.state.items} />
+			</div>
+		);
+	}
+}
+
+export default TodoList;
