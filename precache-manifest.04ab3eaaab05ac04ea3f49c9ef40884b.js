217a0c1813c52911dfbb54b033817f6b7cd1d7a5
jocvegar <jocvegar@gmail.com>
2019-04-20 18:27:11 -0600

    Updates

---
+++ precache-manifest.04ab3eaaab05ac04ea3f49c9ef40884b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c43398dda0928c85281",
+    "url": "/todo_list_app_react/static/js/runtime~main.3c43398d.js"
+  },
+  {
+    "revision": "07b8dcd26ab13b4309ab",
+    "url": "/todo_list_app_react/static/js/main.07b8dcd2.chunk.js"
+  },
+  {
+    "revision": "2cfd82824bb64cef857d",
+    "url": "/todo_list_app_react/static/js/1.2cfd8282.chunk.js"
+  },
+  {
+    "revision": "07b8dcd26ab13b4309ab",
+    "url": "/todo_list_app_react/static/css/main.7dc5068d.chunk.css"
+  },
+  {
+    "revision": "391eec4df83735c298e9ef2712356028",
+    "url": "/todo_list_app_react/index.html"
+  }
+];
